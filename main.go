e9faed831b0c67e7e27176805d4e6c1db49d066c
nurendra rifat wicaksono <137755624+Rifatfat@users.noreply.github.com>
2025-05-16 15:55:28 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,6 @@
+// Tubes Alpro
+// Kelompok Topik 20 (Budget Traveling)
+// Nama anggota kelompok :
+// Tiara Fitriannisha
+// Muh Khuba Adla - 1030400327
+// Nurendra Rifat Wicaksono - 103012400279
