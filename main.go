2edd5256dd5edf55d1cf93032acaec88c780269f
Rifat <rifatwicaksono17@gmail.com>
2025-05-26 08:37:53 +0700

    Penambahan nama dan nim

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 // Tubes Alpro
 // Kelompok Topik 20 (Budget Traveling)
 // Nama anggota kelompok :
-// Tiara Fitriannisha
+// Tiara Fitriannisha (103012400015)
 // Muh Khuba Adla - 1030400327
 // Nurendra Rifat Wicaksono - 103012400279
